dab1713e384c3e2b3c7e9ec01fa0719e53186b04
Heather Conley <hconley6695@gmail.com>
2017-07-26 10:58:57 -0400

    appended stars to page. all showing up

--- src/js/index.js
+++ src/js/index.js
@@ -11,52 +11,26 @@ function createStar() {
 	var littleStar = new Star();
 
 	var left = littleStar.coordinates.x + 'px';
-	console.log(left);
 	var top = littleStar.coordinates.y + 'px';
-	console.log(top);
 	var width = littleStar.size.w + 'px';
-	console.log(width);
 	var height = littleStar.size.h + 'px';
-	console.log(height);
+
 	var $div = $('<div class="color star"></div>');
 
-	// $('.night-sky').append($div);	
-	console.log($div);
+	$($div).css("background-color", "white");
+	$($div).css("height", height);
+	$($div).css("width", width);
+	$($div).css("left", left);
+	$($div).css("top", top);
 
-	$("div.color").css("background-color", "white");
-	$("div.color").css("height", height);
-	$("div.color").css("width", width);
-	$("div.color").css("left", left);
-	$("div.color").css("top", top);
-	$('.night-sky').append($div);	
-
+	$('.night-sky').append($div);
 }
-createStar();
-createStar();
-createStar();
 
-// $('.night-sky').append($div);
 
-for (var i = 0; i <= 5; i++) {
+for (var i = 0; i <= 100; i++) {
 
-// 	var littleStar = new Star();
-
-// 	var left = littleStar.coordinates.x + 'px';
-// 	var top = littleStar.coordinates.y + 'px';
-// 	var width = littleStar.size.w + 'px';
-// 	var height = littleStar.size.h + 'px';
-// 	var $div = $('<div class="color star"></div>');
-
-// 	$('.stars').append(littleStar);
-	
-// 	$("div.color").css("background-color", "white");
-// 	$("div.color").css("height", height);
-// 	$("div.color").css("width", width);
-// 	$("div.color").css("left", left);
-// 	$("div.color").css("top", top);
-// 	$('.stars').append($div);	
-
-	// createStar();
-
+	createStar();
 }
+
+
 
